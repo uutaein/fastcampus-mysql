6e673df5cab74bb57074f8760e7e5d8c7a1d9479
utaein <uutaein@gmail.com>
2024-03-09 19:21:18 +0900

    CH04.03 회원정보 조회 구현 (진짜) 저번 커밋은 회원정보 등록 구현이었구연

--- src/main/java/com/example/fastcampusmysql/domain/member/service/MemberWriteService.java
+++ src/main/java/com/example/fastcampusmysql/domain/member/service/MemberWriteService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 public class MemberWriteService {
     final private MemberRepository memberRepository;
 
-    public void create(RegisterMemberCommand command) {
+    public Member register(RegisterMemberCommand command) {
     /*
         목표 : 회원정보(이메일, 닉네임, 생년월일)을 등록한다.
               닉네임은 18자를 넘길 수 없다.
@@ -26,6 +26,6 @@ public class MemberWriteService {
                 .email(command.email())
                 .birthday(command.birthday())
                 .build();
-        memberRepository.save(member);
+        return memberRepository.save(member);
     }
 }
